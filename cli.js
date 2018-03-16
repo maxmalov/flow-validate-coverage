cd8b202512df07864ad9e398380ff75911631ccf
Max Malov <m.malov.v@gmail.com>
2018-03-16 21:24:11 +0600

    Kind of working sample

--- cli.js
+++ cli.js
@@ -2,15 +2,24 @@
 
 const yargs = require('yargs');
 const execa = require('execa');
-const { getStatus } = require('flow-annotation-check');
+const { genCheckFlowStatus } = require('flow-annotation-check');
 
 const argv = yargs
-  .usage('Usage: $0 [options] file')
-  .example('$0 -t 85 ./foo.js', 'Validates foo.js flow coverage against the specified threshold')
-  .alias('t', 'threshold')
-  .describe('t', 'The minimum coverage below which the command will fail')
-  .alias('f', 'flow-path')
-  .describe('f', 'Custom flow binary path')
+  .usage('Usage: $0 [options]')
+  .example('$0 -t 85 -i ./foo.js', 'Validates flow coverage of foo.js is greater or equal to 85%')
+  .option('input', {
+    alias: 'i',
+    describe: 'Load this file'
+  })
+  .option('threshold', {
+    alias: 't',
+    describe: 'The minimum coverage below which the command will fail'
+  })
+  .option('flow-path', {
+    alias: 'f',
+    describe: 'Custom flow binary path'
+  })
+  .demandOption(['threshold', 'input'])
   .help()
   .argv;
 
@@ -23,16 +32,17 @@ function error(msg) {
 
 (async () => {
   try {
-    const flowStatus = await getStatus(file);
+    const flowBin = flowPath || 'flow';
+    const flowStatus = await genCheckFlowStatus(flowBin, file);
 
     // skip files w/o flow annotation
     if (flowStatus === 'no flow') {
       return;
     }
 
-    const { stdout } = await execa(flowPath || 'flow', ['coverage', file, '--json']);
+    const { stdout } = await execa(flowBin, ['coverage', file, '--json']);
     const { expressions: { covered_count, uncovered_count} } = JSON.parse(stdout);
-    const coverage = covered_count / (covered_count + uncovered_count);
+    const coverage = (covered_count / (covered_count + uncovered_count)) * 100;
 
     if (coverage < threshold) {
       error(`${file}: coverage ${coverage.toFixed(2)}% is below the specified threshold ${threshold}%`);
